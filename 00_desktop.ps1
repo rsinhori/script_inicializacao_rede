6796001c088fcc59225fd020782d6b3e2fcafae3
rsinhori <89921840+rsinhori@users.noreply.github.com>
2021-09-01 11:05:58 -0300

    Add files via upload

---
+++ 00_desktop.ps1
@@ -0,0 +1,9 @@
+﻿cls
+#Pega o serial da maquina e adiciona o N 
+$serial = (Get-CimInstance Win32_BIOS).SerialNumber
+$nome = "D$serial"
+
+#Adiciona a maquina no dominio
+Add-Computer -DomainName "gel-eng.local" -NewName $nome -Credential gel-eng\authenticator -Restart
+
+
