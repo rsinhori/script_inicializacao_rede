4c4cd6a38b9cb9d32dd3251b5337d6294133ceae
rsinhori <89921840+rsinhori@users.noreply.github.com>
2021-09-01 11:07:25 -0300

    Update 00_desktop.ps1

--- 00_desktop.ps1
+++ 00_desktop.ps1
@@ -4,6 +4,6 @@ $serial = (Get-CimInstance Win32_BIOS).SerialNumber
 $nome = "D$serial"
 
 #Adiciona a maquina no dominio
-Add-Computer -DomainName "gel-eng.local" -NewName $nome -Credential gel-eng\authenticator -Restart
+Add-Computer -DomainName "gel-eng.local" -NewName $nome -Restart
 
 
